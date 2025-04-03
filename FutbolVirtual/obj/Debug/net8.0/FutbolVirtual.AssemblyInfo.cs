d611e7a37c7106aa9e64c0e3bee34e9d9113d0be
LuzIbarra7 <luzmabel.ibarraet12de1@gmail.com>
2025-04-03 15:52:27 -0300

    commit
    Ejercicio nuevo

--- FutbolVirtual/obj/Debug/net8.0/FutbolVirtual.AssemblyInfo.cs
+++ FutbolVirtual/obj/Debug/net8.0/FutbolVirtual.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FutbolVirtual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fcf7b59828a46597cd205c0352311cc1033b84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a451190b2175bc72a6eec08c2ab4f7ba8f36d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FutbolVirtual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FutbolVirtual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
